d202a30954a4c5d2ff5bf0ff7c7324732ea0f4bd
ErickHCerecedo <erickcerecedo@gmail.com>
2026-05-21 23:22:32 -0600

    actualizacion en cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,9 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => array_filter(
+        array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', env('FRONTEND_URL', 'http://localhost:3000'))))
+    ),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
