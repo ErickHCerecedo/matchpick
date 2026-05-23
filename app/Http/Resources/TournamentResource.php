13a55ff7e3ad0e86fb7e2624c0b951569e4c8b89
ErickHCerecedo <erickcerecedo@gmail.com>
2026-05-23 00:59:10 -0600

    Add is_active field to TournamentResource response

--- app/Http/Resources/TournamentResource.php
+++ app/Http/Resources/TournamentResource.php
@@ -15,6 +15,7 @@ class TournamentResource extends JsonResource
             'type' => $this->type,
             'season' => $this->season,
             'logo_url' => $this->logo_url,
+            'is_active' => (bool) $this->is_active,
             'starts_at' => $this->starts_at?->toDateString(),
             'ends_at' => $this->ends_at?->toDateString(),
             'rounds_count' => $this->whenLoaded('rounds', fn() => $this->rounds->count()),
